95b0835ca94dd111fd8eeae0cf01379b98cb7c19
MbadaShehade <157805443+MbadaShehade@users.noreply.github.com>
2024-08-01 12:10:37 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/react-tailwindcss-select/dist/index.esm.js"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react-tailwindcss-select/dist/index.esm.js",
+  ],
   darkMode: "class",
   theme: {
     extend: {
       colors: {
-        // customDark: "#1C212F",
-        customDark: "#1B212F",
+        customDark: "#1C212F",
         customBlue: "#F7FDFE",
       },
     },
